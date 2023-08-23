21ae9e220235d684c733cd8b330b4bb4f0992171
Symeon Melnychuk <m.simeon998@gmail.com>
2023-08-23 12:07:35 +0300

    2

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,10 +2,16 @@ const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   const func = (req, _, next) => {
+    if (Object.keys(req.body).length === 0) {
+      const error = new Error("missing fields");
+      error.status = 400;
+      return next(error);
+    }
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, "missing fields"));
+      next(HttpError(400, error.message));
     }
+
     next();
   };
   return func;
